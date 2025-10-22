16218084cc5c19cdce301a4848ba69aaf9edd966
Ashton <averheye@asu.edu>
2025-10-22 09:31:32 -0700

    Eggs file score

--- src/pages/eggs.js
+++ src/pages/eggs.js
@@ -19,11 +19,14 @@ class EggsPage extends Page {
     super.enter();
     rectMode(CENTER);
     textAlign(CENTER, CENTER);
+    
   }
 
   show() {
     image(backgroundImg, 0, 0, canvas.x, canvas.y);
     drawGameTitle({ title: "Eggs", widthOffset: 90, yOffset: -20 });
     this.backButton.show();
+    let score = 0;
+    text("Score: "+score, 45, 55);
   }
 }
