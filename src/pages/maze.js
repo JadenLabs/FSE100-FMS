a9558d34a799ff673a2f4a37e47d6a431ca16e9a
abdou <agannoun@asu.edu>
2025-10-27 16:54:35 -0700

    maze pixle detection

--- src/pages/maze.js
+++ src/pages/maze.js
@@ -22,8 +22,32 @@ class MazePage extends Page {
   }
 
   show() {
-    image(backgroundImg, 0, 0, canvas.x, canvas.y);
-    image(mazebg, 0, 0, canvas.x, canvas.y);
+  
+  image(backgroundImg, 0, 0, canvas.x, canvas.y);
+  image(mazebg, 0, 0, canvas.x, canvas.y);
+
+  
+  push();
+  imageMode(CENTER);
+  const mazeWidth = canvas.x * 0.4;
+  const mazeHeight = canvas.y * 0.55;
+  image(maze1, canvas.x / 2, canvas.y / 2, mazeWidth, mazeHeight);
+  pop(); 
+
+  
+  const mx = constrain(mouseX, 0, canvas.x - 1);
+  const my = constrain(mouseY, 0, canvas.y - 1);
+  const c = get(mx, my);
+
+ 
+  const isWall = red(c) < 50 && green(c) < 50 && blue(c) < 50;
+
+  
+  
+  stroke(255);          
+  strokeWeight(1);
+  fill(isWall ? "red" : "orange");
+  circle(mx, my, 13);
 
 
    
